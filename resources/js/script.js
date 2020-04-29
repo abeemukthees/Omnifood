f68b4b278263ea0c97dc13f36cab69f5d63d8b33
Abhi Muktheeswarar <abhi.muktheeswarar@redbus.com>
2020-04-29 23:16:04 +0530

    Setup jQuery animations

---
+++ resources/js/script.js
@@ -0,0 +1,104 @@
+$(document).ready(function() {
+
+    //For sticky navigation
+    new Waypoint({
+        element: document.getElementsByClassName("js--section-features"),
+        handler: function(direction) {
+            if (direction == "down") {
+                $("nav").addClass("sticky");
+            } else {
+                $("nav").removeClass("sticky");
+            }
+        },
+        offset: '60px'
+    })
+
+
+    //Scroll on buttons
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
+    });
+
+
+    //Navigation scroll
+    $(function() {
+        $('a[href*="#"]')
+            // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function(event) {
+                // On-page links
+                if (
+                    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                    location.hostname == this.hostname
+                ) {
+                    // Figure out element to scroll to
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    // Does a scroll target exist?
+                    if (target.length) {
+                        // Only prevent default if animation is actually gonna happen
+                        event.preventDefault();
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, 1000, function() {
+                            // Callback after animation
+                            // Must change focus!
+                            var $target = $(target);
+                            $target.focus();
+                            if ($target.is(":focus")) { // Checking if the target was focused
+                                return false;
+                            } else {
+                                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                $target.focus(); // Set focus again
+                            };
+                        });
+                    }
+                }
+            });
+    });
+
+
+    //Animations on scroll
+
+    new Waypoint({
+        element: document.getElementsByClassName("js--wp-1"),
+        handler: function(direction) {
+            console.log(direction);
+            $('.js--wp-1').addClass('animated fadeIn');
+        },
+        offset: '50%'
+    });
+
+    new Waypoint({
+        element: document.getElementsByClassName("js--wp-2"),
+        handler: function(direction) {
+            console.log(direction);
+            $('.js--wp-2').addClass('animated fadeInUp');
+        },
+        offset: '50%'
+    });
+
+    new Waypoint({
+        element: document.getElementsByClassName("js--wp-3"),
+        handler: function(direction) {
+            console.log(direction);
+            $('.js--wp-3').addClass('animated fadeIn');
+        },
+        offset: '50%'
+    });
+
+    new Waypoint({
+        element: document.getElementsByClassName("js--wp-4"),
+        handler: function(direction) {
+            console.log(direction);
+            $('.js--wp-4').addClass('animated pulse');
+        },
+        offset: '50%'
+    });
+
+});
